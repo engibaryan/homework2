6f8c641ec6bc09ac32c72139271abcc752d3243e
engibaryan <xachik.engibar@gmail.com>
2018-09-05 21:49:46 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,24 @@
+const a = 5
+const b = "Italakan"
+const c = 12
+const v = 12/5;
+const d = prompt("Stex indznic tver en uzum. Mi ban asa (tiv)");
+if (isNaN(d)){
+
+	alert("Tiv gri.");
+}
+else {
+	alert("Shnorhakal em.  Ete ban chberi kmtnes console.");
+}
+const k = (5+12) - d;
+
+if(k <= 0) {
+
+	alert("lave mec tiv asecir.");
+	console.log(d);
+}
+else {
+   console.log("krvanq");
+   console.log(d);
+
+}
