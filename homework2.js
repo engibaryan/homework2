1e152f460a11dee7829351677f3a99150cfc72fe
engibaryan <43010406+engibaryan@users.noreply.github.com>
2018-09-12 20:10:36 +0400

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -2,23 +2,23 @@ const a = 5
 const b = "Italakan"
 const c = 12
 const v = 12/5;
-const d = prompt("Stex indznic tver en uzum. Mi ban asa (tiv)");
+const d = prompt("Number Please");
 if (isNaN(d)){
 
-	alert("Tiv gri.");
+	alert("I have said NUMBER -_-");
 }
 else {
-	alert("Shnorhakal em.  Ete ban chberi kmtnes console.");
+	alert("Thanks.");
 }
 const k = (5+12) - d;
 
 if(k <= 0) {
 
-	alert("lave mec tiv asecir.");
+	alert("Nice Job");
 	console.log(d);
 }
 else {
-   console.log("krvanq");
+   console.log("calculus unem anelu");
    console.log(d);
 
 }
